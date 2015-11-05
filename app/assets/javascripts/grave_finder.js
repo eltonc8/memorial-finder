11eda3095215fa24b857893390204556e9f02959
eltonc88 <eltonchan@cal.berkeley.edu>
2015-11-05 14:24:36 -0800

    add basic structure-backbone

---
+++ app/assets/javascripts/grave_finder.js
@@ -0,0 +1,12 @@
+window.GraveFinder = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+  }
+};
+
+$(document).ready(function(){
+  GraveFinder.initialize();
+});
